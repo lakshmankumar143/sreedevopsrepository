84f5ac034329b5890d10acfde293acf4e338af3e
Testuser1 <lakshmankumar143@gmail.com>
2020-10-06 07:29:40 +0000

    added new script like if config

--- details.sh
+++ details.sh
@@ -3,3 +3,12 @@ echo "This script will give system information"
 echo $(hostname)
 echo $(whoami)
 
+#Gives users list
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS
+echo 
+#GET IP ADDRESS OF THE SYSTEM
+echo 
+IP=$(ifconfig)
+echo $IP
+
